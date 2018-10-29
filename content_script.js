44b6f64ec93dbd3ba884a89ed4028105d8408985
carolinezhao <chaohsimeng@gmail.com>
2018-10-29 23:14:05 +0800

    feat: change api; add ux; update input format

--- content_script.js
+++ content_script.js
@@ -1,28 +1,24 @@
 /* Check if microphone is enabled */
 
-navigator.webkitGetUserMedia({
-    audio: true
-}, s => {
-    console.log('Microphone is ready. Click on textbox and speak.');
-}, err => {
-    alert('Please enable Microphone Permission.');
-});
+navigator.mediaDevices.getUserMedia({
+        audio: true
+    })
+    .then(stream => {
+        console.log('Microphone is ready.');
+    })
+    .catch(err => {
+        alert('[来自 Speech To Text 的提示] 如果您想在网页中使用语音输入，请开启麦克风权限：\n方法1：点击地址栏中的摄像机图标，选择允许。\n方法2：到 chrome://settings/content/microphone 中设置。');
+        console.log(err);
+    });
 
 
 /* Get <input> nodes of the current page */
 
-var targetNode; // 目标输入框
+var targetNode; // 目标文本框
+var originColor;
 
 if (document) {
-    getNodes();
-}
-
-function getNodes() {
-    var inputNodes = document.querySelectorAll(createSelector());
-    // console.log(inputNodes);
-    if (inputNodes.length) {
-        bindEvent(inputNodes);
-    }
+    bindEvent();
 }
 
 function createSelector() {
@@ -38,14 +34,26 @@ function createSelector() {
     return selector;
 }
 
-function bindEvent(array) {
-    array.forEach(item => {
-        item.onfocus = event => {
-            startRecord(event);
-            targetNode = event.target;
-            targetNode.value = 'speak...' // 语音识别有结果后重写 value
-        };
-    })
+function bindEvent() {
+    var inputNodes = document.querySelectorAll(createSelector());
+    // console.log(inputNodes);
+    if (inputNodes.length) {
+        inputNodes.forEach(item => {
+            item.onfocus = event => {
+                startRecord(event);
+                targetNode = event.target;
+            };
+        })
+    }
+}
+
+function highlightStyle(node) {
+    originColor = node.style.backgroundColor;
+    node.style.backgroundColor = '#DEE8FC';
+}
+
+function restoreStyle(node) {
+    node.style.backgroundColor = originColor;
 }
 
 
@@ -56,17 +64,19 @@ var recognizing = false;
 var ignore_onend;
 var start_timestamp;
 
-if ('webkitSpeechRecognition' in window) {
+if ('webkitSpeechRecognition' in window) { // already checked in extension
     var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
+    recognition.continuous = false; // config
     recognition.interimResults = true;
 
     recognition.onstart = function () {
         recognizing = true;
+        highlightStyle(targetNode);
     };
     recognition.onerror = function (event) {};
     recognition.onend = function () {
         recognizing = false;
+        restoreStyle(targetNode);
         if (ignore_onend) {
             return;
         }
@@ -81,10 +91,14 @@ if ('webkitSpeechRecognition' in window) {
                 interim_transcript += event.results[i][0].transcript;
             }
         }
-        final_transcript = capitalize(final_transcript);
+        final_transcript = linebreak(capitalize(final_transcript));
         // console.log(interim_transcript);
-        // targetNode.value = linebreak(interim_transcript); // doesn't work
-        targetNode.value = linebreak(final_transcript);
+        // targetNode.value = linebreak(interim_transcript); // doesn't work     
+        if (targetNode.value.length !== 0 && final_transcript) {
+            targetNode.value += ` ${final_transcript}`;
+        } else {
+            targetNode.value += final_transcript;
+        }
     };
 }
 
@@ -111,7 +125,7 @@ function startRecord(event) {
     // recognition.lang = select_dialect.value;
     recognition.start();
     ignore_onend = false;
-    event.target.value = '';
+    // event.target.value = ''; // config
     // interim_span.value = '';
     start_timestamp = event.timeStamp;
 }
