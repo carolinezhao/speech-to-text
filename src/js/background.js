16bd1deb0d10d039575c54c9819409e5dc78478e
carolinezhao <chaohsimeng@gmail.com>
2018-11-01 20:50:01 +0800

    chore: update img; revise text.

--- src/js/background.js
+++ src/js/background.js
@@ -2,7 +2,5 @@ import '../img/icon-128.png'
 import '../img/icon-32.png'
 
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.local.remove(['micStatus', 'ifContinuous'], function (data) {
-    console.log(data);
-  });
+  // chrome.storage.local.clear(data => console.log(data));
 });
