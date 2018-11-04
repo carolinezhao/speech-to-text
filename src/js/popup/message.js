fea08c78539356e66ed3be704b1dedf0f7bdff57
carolinezhao <chaohsimeng@gmail.com>
2018-11-04 16:46:02 +0800

    feat: add styles for different button statuses

--- src/js/popup/message.js
+++ src/js/popup/message.js
@@ -7,7 +7,7 @@ export function message() {
     noSpeech: '没有检测到语音。',
     upgrade: '您使用的浏览器版本不支持语音识别功能。请升级到 Chrome 25 或更高版本。',
     noMicrophone: '没有检测到麦克。请检查麦克是否正确安装。',
-    blocked: '您禁用了麦克使用权限。请到 Options 页面修改权限。',
-    denied: '您拒绝了麦克使用权限。请到 Options 页面修改权限。',
+    blocked: '您禁用了插件的麦克使用权限。请到 Options 页面修改权限。',
+    denied: '您拒绝了插件的麦克使用权限。请到 Options 页面修改权限。',
   }
 }
