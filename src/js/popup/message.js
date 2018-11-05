6422cac26b3f942a06d0fd6ca6e85803fdd723b4
carolinezhao <chaohsimeng@gmail.com>
2018-11-05 20:15:22 +0800

    feat: make textbox scroll when input overflow. refactor: update field; merge components.

--- src/js/popup/message.js
+++ src/js/popup/message.js
@@ -1,13 +1,11 @@
-export function message() {
-  return {
-    enableMic: '在插件中使用，请到 Options 页面开启麦克权限。',
-    start: '点击麦克图标开始录音。',
-    speak: '请讲话。点击图标结束。',
-    copy: '使用快捷键 Command+C (Mac) / Ctrl+C (Win) 复制文本。',
-    noSpeech: '没有检测到语音。',
-    upgrade: '您使用的浏览器版本不支持语音识别功能。请升级到 Chrome 25 或更高版本。',
-    noMicrophone: '没有检测到麦克。请检查麦克是否正确安装。',
-    blocked: '您禁用了插件的麦克使用权限。请到 Options 页面修改权限。',
-    denied: '您拒绝了插件的麦克使用权限。请到 Options 页面修改权限。',
-  }
+export let message = {
+  enableMic: '在插件中使用，请到 Options 页面开启麦克权限。',
+  start: '点击麦克图标开始录音。',
+  speak: '请讲话。点击图标结束。',
+  copy: '使用快捷键 Command+C (Mac) / Ctrl+C (Win) 复制文本。',
+  noSpeech: '没有检测到语音。',
+  upgrade: '您使用的浏览器版本不支持语音识别功能。请升级到 Chrome 25 或更高版本。',
+  noMicrophone: '没有检测到麦克。请检查麦克是否正确安装。',
+  blocked: '您禁用了插件的麦克使用权限。请到 Options 页面修改权限。',
+  denied: '您拒绝了插件的麦克使用权限。请到 Options 页面修改权限。',
 }
--- src/js/popup/recognition.js
+++ src/js/popup/recognition.js
@@ -18,3 +18,16 @@ export function initRecognition(env) {
   // console.log(recognition);
   return recognition;
 }
+
+export function capitalize(s) {
+  var first_char = /\S/;
+  return s.replace(first_char, function (m) {
+    return m.toUpperCase();
+  });
+}
+
+export function linebreak(s) {
+  var two_line = /\n\n/g;
+  var one_line = /\n/g;
+  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
