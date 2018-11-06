189386e51220e2569527595a5912755df94611d5
carolinezhao <chaohsimeng@gmail.com>
2018-11-06 12:53:21 +0800

    fix: when reloading a tab, the tab status remained

--- src/js/background.js
+++ src/js/background.js
@@ -8,6 +8,21 @@ chrome.runtime.onInstalled.addListener(() => {
   // chrome.storage.local.remove(['micStatus'], data => console.log(data));
 });
 
+/* When reloading a tab, remove the tab status and reload the extension */
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabs) => {
+  chrome.storage.local.get(['workInTab'], data => {
+    let workInTab = data.workInTab;
+    if (`${tabId}` in workInTab && workInTab[tabId]) {
+      delete workInTab[tabId];
+      chrome.runtime.reload();
+      // console.log(workInTab);
+    }
+  });
+})
+
+/* When closing a tab, remove the tab status */
+
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
   // console.log(tabId);
   chrome.storage.local.get(['workInTab'], data => {
